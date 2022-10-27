830c53fc9ae392210f2b31ee72e753f15a802aca
ivan-My <252511374@qq.com>
2022-10-27 17:52:50 +0800

    ✨新增(leetcode): test
    test
    
    ISSUES CLOSED: test

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
-// babel.config.js
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react', // 追加
+    '@babel/preset-react',
     '@babel/preset-typescript',
   ],
 }
