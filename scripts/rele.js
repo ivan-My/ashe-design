285f3def6bf0abcaf04d3cc394becb21cd25781c
ivan-My <252511374@qq.com>
2023-05-28 12:46:45 +0800

    chore: 0000

--- scripts/rele.js
+++ scripts/rele.js
@@ -1,46 +1,38 @@
+const fs = require('fs')
 const inquirer = require('inquirer')
 const { version } = require('../package.json')
+const semverInc = require('semver/functions/inc')
 
-function incrementVersion(version, incrementType) {
-  const [major, minor, patch] = version.split('.').map(Number)
-  switch (incrementType) {
-    case 'major':
-      return `${major + 1}.0.0`
-    case 'minor':
-      return `${major}.${minor + 1}.0`
-    case 'patch':
-      return `${major}.${minor}.${patch + 1}`
-    default:
-      throw new Error(`无效的增量类型：${incrementType}`)
+const getNextVersion = (currentVersion) => {
+  return {
+    major: semverInc(currentVersion, 'major'),
+    minor: semverInc(currentVersion, 'minor'),
+    patch: semverInc(currentVersion, 'patch'),
+    premajor: semverInc(currentVersion, 'premajor'),
+    preminor: semverInc(currentVersion, 'preminor'),
+    prepatch: semverInc(currentVersion, 'prepatch'),
+    prerelease: semverInc(currentVersion, 'prerelease'),
   }
 }
 
-const majorVersion = incrementVersion(version, 'major')
-const minorVersion = incrementVersion(version, 'minor')
-const patchVersion = incrementVersion(version, 'patch')
-
+const nextVersion = getNextVersion(version)
 inquirer
   .prompt([
     {
       type: 'list',
       name: 'name',
       message: `请选择要发布的版本，当前版本为:${version}`,
-      choices: [
-        {
-          name: `major => ${majorVersion}`,
-          value: majorVersion,
-        },
-        {
-          name: `minor => ${minorVersion}`,
-          value: minorVersion,
-        },
-        {
-          name: `patch => ${patchVersion}`,
-          value: patchVersion,
-        },
-      ],
+      choices: Object.keys(nextVersion).map((item) => {
+        return {
+          name: `${item}  => ${nextVersion[item]}`,
+          value: nextVersion[item],
+        }
+      }),
     },
   ])
   .then(function (answers) {
     console.log(answers)
+    // if (answers.name === 3) {
+    //   console.log(222)
+    // }
   })
