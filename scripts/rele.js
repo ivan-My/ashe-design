a48a0cd4a29afd1df744ec5a9a0710513ebc75d3
ivan-My <252511374@qq.com>
2023-05-28 12:26:15 +0800

    chore: 222

--- scripts/rele.js
+++ scripts/rele.js
@@ -1,17 +1,44 @@
 const inquirer = require('inquirer')
-const semver = require('semver')
 const { version } = require('../package.json')
-const patch = require('semver/functions/patch')
-const minVersion = require('semver/ranges/min-version')
-console.log(semver.minVersion(version))
-return
+
+function incrementVersion(version, incrementType) {
+  const [major, minor, patch] = version.split('.').map(Number)
+  switch (incrementType) {
+    case 'major':
+      return `${major + 1}.0.0`
+    case 'minor':
+      return `${major}.${minor + 1}.0`
+    case 'patch':
+      return `${major}.${minor}.${patch + 1}`
+    default:
+      throw new Error(`无效的增量类型：${incrementType}`)
+  }
+}
+
+const majorVersion = incrementVersion(version, 'major')
+const minorVersion = incrementVersion(version, 'minor')
+const patchVersion = incrementVersion(version, 'patch')
+
 inquirer
   .prompt([
     {
       type: 'list',
       name: 'name',
       message: `请选择要发布的版本，当前版本为:${version}`,
-      choices: [`major => ${version}`, '222'],
+      choices: [
+        {
+          name: `major => ${majorVersion}`,
+          value: majorVersion,
+        },
+        {
+          name: `minor => ${minorVersion}`,
+          value: minorVersion,
+        },
+        {
+          name: `patch => ${patchVersion}`,
+          value: patchVersion,
+        },
+      ],
     },
   ])
   .then(function (answers) {
