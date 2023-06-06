2aa21489d72f9fdde0d80e62df7546857b227261
ivan-My <252511374@qq.com>
2023-06-06 09:29:15 +0800

    chore: 准备修复from

--- re.js
+++
@@ -1,6 +0,0 @@
-//import defaultRelaseProcess from '@mx-design/release'
-
-const defaultRelaseProcess = require('@mx-design/release')
-//console.log(defaultRelaseProcess)
-
-defaultRelaseProcess.default()
