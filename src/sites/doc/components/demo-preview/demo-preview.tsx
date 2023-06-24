97d533b5e0e1bd4ce7250c34d36bca1b8a60192a
ivan-My <252511374@qq.com>
2023-06-24 23:09:22 +0800

    chore: 000

--- src/sites/doc/components/demo-preview/demo-preview.tsx
+++ src/sites/doc/components/demo-preview/demo-preview.tsx
@@ -5,11 +5,12 @@ import './demo-preview.scss'
 const DemoPreview = () => {
   const { pathname } = useLocation()
   // @ts-ignore
+  const mode = import.meta.env.MODE
   const url =
-    import.meta.env.MODE === 'development'
+    mode === 'development'
       ? `/demo.html#${pathname}`
       : `https://ivan-my.github.io/ashe-design/demo/demo.html#${pathname}`
-  console.log(url)
+  console.log('url=', url)
   return (
     <div className="doc-demo-preview">
       <iframe src={url} frameBorder="0"></iframe>
