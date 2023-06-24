e7295dc7a409da513ef4a07edc41de6ed750e6cb
ivan-My <252511374@qq.com>
2023-06-24 23:00:59 +0800

    chore: 修复了iframe路径

--- src/sites/doc/components/demo-preview/demo-preview.tsx
+++ src/sites/doc/components/demo-preview/demo-preview.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import './demo-preview.scss'
 
 const DemoPreview = () => {
-    const {pathname}= useLocation()
-    return (
-        <div className='doc-demo-preview'>
-            <iframe src={`/demo.html#${pathname}`} frameBorder="0"></iframe>
-        </div>
-    )
+  const { pathname } = useLocation()
+  // @ts-ignore
+  const url =
+    import.meta.env.MODE === 'development'
+      ? `/demo.html#${pathname}`
+      : `https://ivan-my.github.io/ashe-design/demo/demo.html#${pathname}`
+  return (
+    <div className="doc-demo-preview">
+      <iframe src={url} frameBorder="0"></iframe>
+    </div>
+  )
 }
 
 export default DemoPreview
